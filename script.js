8f612ac947ea4804ffda5fb07b59aa2b01f0977c
Vera D <veradanicode@gmail.com>
2025-10-11 10:27:35 +0100

    add open and close function for hambagur menu

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuOpenButton=document.querySelector("#menu-open-button");
+const menuCloseButton=document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click",()=>{
+    //Toogle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu")
+});
+
+menuCloseButton.addEventListener("click",()=>menuOpenButton.click());
