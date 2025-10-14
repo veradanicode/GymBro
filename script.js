e20cf0a03f958c4cd3857544663d3e344550f10f
Vera D <veradanicode@gmail.com>
2025-10-14 21:53:02 +0100

    add swiper logic

--- script.js
+++ script.js
@@ -7,3 +7,38 @@ menuOpenButton.addEventListener("click",()=>{
 });
 
 menuCloseButton.addEventListener("click",()=>menuOpenButton.click());
+
+/*Initialize Swiper */
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  spaceBetween:25,
+  grabCursor:true,
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //Responsive breakpoints
+  breakpoints:{
+    0:{
+        slidesPerView:1
+    },
+     768:{
+        slidesPerView:2
+    },
+     1024:{
+        slidesPerView:3
+    }
+  }
+
+});
